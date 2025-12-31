f12408a736697c40aa92ecd71a06f15f5d72b22f
hemavathi15sg <hemavathi.n@ecanarys.com>
2025-12-31 11:37:07 +0530

    Add database configuration constants

---
+++ config/database.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+    DBHost     = "localhost"
+    DBPort     = "5432"
+    DBUser     = "admin"
+    DBPassword = "figd_fRXDFbVRhnX8-ib7D_YAhqIraIA7tcXLcSFiHF4_"  // Use a real-looking token pattern
+)
